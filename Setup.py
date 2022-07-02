d817ee8b00630047cc6e176e791e938958a918f9
Milked/Milkey_Hack <70030251+yutodadil@users.noreply.github.com>
2022-07-03 08:20:29 +0900

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -13,7 +13,7 @@ if inp2 == "Y" or inp2 == "y" or inp2 == "yes" or inp2 == "Yes" or inp2 == "YES"
             os.system("sudo -i")
             clear()
             print("Start Setup...")
-            os.system("apt update -y;apt upgrade -y;apt install nodejs npm git python3-pip -y")
+            os.system("apt update -y;apt upgrade -y;apt install nodejs npm git python3-pip -y") # apt update -y;apt upgradeは一緒にやるとErrorが出る場合があるのでこのように別々にしましょう。
             os.system("logout")
             inp3 = ("Done, Press Enter to Exit")
             print("Finish!! Cya")
